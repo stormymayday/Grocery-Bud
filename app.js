87cfea5f8aaa568b3b94541e6074a8523ea2f5cb
stormymayday <stormymayday7@gmail.com>
2023-03-18 19:25:16 -0600

    Selecting the 'grocery-container'

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const groceryInput = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
-console.log(submitBtn);
+const groceryContainer = document.querySelector('.grocery-container');
+console.log(groceryContainer);
