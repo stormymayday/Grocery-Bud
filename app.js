4ee775818edb837724d5170334a125483649790b
stormymayday <stormymayday7@gmail.com>
2023-03-19 21:11:27 -0600

    Checking if the length of the items nodeList is greater than 0

--- app.js
+++ app.js
@@ -143,6 +143,12 @@ function clearItems() {
     // Selecting all the 'grocery-item's
     const items = document.querySelectorAll('.grocery-item');
 
+    // Checking if the length of the nodeList is > 0
+    if (items.length > 0) {
+
+
+    }
+
 }
 
 // ********** Local Storage **********
