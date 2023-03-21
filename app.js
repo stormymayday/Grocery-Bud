09fb1cc318206be2f846d0e1b1f6792008cb540c
stormymayday <stormymayday7@gmail.com>
2023-03-20 20:52:57 -0600

    IF the item ID matches the ID THEN change the value and return the item. ELSE simply return the item.

--- app.js
+++ app.js
@@ -305,6 +305,20 @@ function editLocalStorage(id, value) {
     // Getting items from the Local Storage
     let items = getLocalStorage();
 
+
+    items = items.map(function (item) {
+
+        // IF the item ID matches the ID THEN change the value and return
+        // ELSE simply return the item
+        if (item.id === id) {
+            item.value = value;
+            return item;
+        } else {
+            return item;
+        }
+
+    });
+
 }
 // Edit in local storage - end
 
