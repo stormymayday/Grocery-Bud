dc28044182fc6c3b564376d3385c170f4f96e599
stormymayday <stormymayday7@gmail.com>
2023-03-20 21:12:53 -0600

    Moved the logic starting from 'Creating an article' up to 'Adding article to the list' from the addItem function to the createListItem function.

--- app.js
+++ app.js
@@ -38,53 +38,7 @@ function addItem(event) {
         // AND there is no editing
         // THEN add item to the list
 
-        // Creating an article
-        const element = document.createElement('article');
 
-        // Adding the 'grocery-item' class
-        element.classList.add('grocery-item');
-
-        // Adding the ID
-        const attribute = document.createAttribute('data-id');
-        attribute.value = id;
-        element.setAttributeNode(attribute);
-
-        // Setting up the innerHTML
-        element.innerHTML = `                    <span class="title">${inputValue}</span>
-
-                    <!-- btn-container start -->
-                    <div class="btn-container">
-
-                        <!-- edit btn start -->
-                        <button type="button" class="edit-btn">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                        <!-- edit btn end -->
-
-                        <!-- delete btn start -->
-                        <button type="button" class="delete-btn">
-                            <i class="fas fa-trash"></i>
-                        </button>
-                        <!-- delete btn end -->
-
-                    </div>
-                    <!-- btn-container end -->
-        `;
-
-        // Targetting the delete item button
-        const deleteBtn = element.querySelector('.delete-btn');
-
-        // Targetting the edit item button
-        const editBtn = element.querySelector('.edit-btn');
-
-        // delete item button event listener
-        deleteBtn.addEventListener('click', deleteItem);
-
-        // edit item button event listener
-        editBtn.addEventListener('click', editItem);
-
-        // Adding article to the list
-        groceryList.appendChild(element);
 
         // Displaying the 'success' alert
         displayAlert('item added to the list', 'success');
@@ -354,6 +308,54 @@ function setUpItems() {
 
 // createListItem - start
 function createListItem(id, value) {
+
+    // Creating an article
+    const element = document.createElement('article');
+
+    // Adding the 'grocery-item' class
+    element.classList.add('grocery-item');
+
+    // Adding the ID
+    const attribute = document.createAttribute('data-id');
+    attribute.value = id;
+    element.setAttributeNode(attribute);
+
+    // Setting up the innerHTML
+    element.innerHTML = `                    <span class="title">${inputValue}</span>
+
+                    <!-- btn-container start -->
+                    <div class="btn-container">
+
+                        <!-- edit btn start -->
+                        <button type="button" class="edit-btn">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <!-- edit btn end -->
+
+                        <!-- delete btn start -->
+                        <button type="button" class="delete-btn">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        <!-- delete btn end -->
+
+                    </div>
+                    <!-- btn-container end -->
+        `;
+
+    // Targetting the delete item button
+    const deleteBtn = element.querySelector('.delete-btn');
+
+    // Targetting the edit item button
+    const editBtn = element.querySelector('.edit-btn');
+
+    // delete item button event listener
+    deleteBtn.addEventListener('click', deleteItem);
+
+    // edit item button event listener
+    editBtn.addEventListener('click', editItem);
+
+    // Adding article to the list
+    groceryList.appendChild(element);
 
 }
 // createListItem - end
