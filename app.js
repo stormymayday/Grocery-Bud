fc92a5e4fc2b2fe50ba815656bb6ca0c21871f68
stormymayday <stormymayday7@gmail.com>
2023-04-28 22:39:31 -0600

    Selecting alert using getElement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import getElement from "./utils/getElement.js";
 
 // ********** Selecting Items **********
-const alert = document.querySelector('.alert');
+const alert = getElement('.alert');
 const form = document.querySelector('.grocery-form');
 const groceryInput = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
