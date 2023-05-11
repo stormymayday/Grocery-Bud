3488d84355c48b3d30b554160f05a3837cbd5af8
stormymayday <stormymayday7@gmail.com>
2023-05-11 12:39:48 -0700

    Moved removeFromLocalStorage.js into a separate module

--- app.js
+++ app.js
@@ -33,23 +33,7 @@ window.addEventListener('DOMContentLoaded', setUpItems);
 // ********** Functions **********
 
 // Remove from local storage - start
-export function removeFromLocalStorage(id) {
 
-    // Getting items from the Local Storage
-    let items = getLocalStorage();
-
-    // Filtering out the item with matching ID
-    items = items.filter(function (item) {
-
-        if (item.id !== id) {
-            return item;
-        }
-
-    });
-
-    // Putting updated 'items' array (w/o the filtered out item) into the Local Storage
-    localStorage.setItem('list', JSON.stringify(items));
-}
 // Remove from local storage - end
 
 // Edit in local storage - start
---
+++ utils/localStorage/removeFromLocalStorage.js
@@ -0,0 +1,17 @@
+export default function removeFromLocalStorage(id) {
+
+    // Getting items from the Local Storage
+    let items = getLocalStorage();
+
+    // Filtering out the item with matching ID
+    items = items.filter(function (item) {
+
+        if (item.id !== id) {
+            return item;
+        }
+
+    });
+
+    // Putting updated 'items' array (w/o the filtered out item) into the Local Storage
+    localStorage.setItem('list', JSON.stringify(items));
+}
