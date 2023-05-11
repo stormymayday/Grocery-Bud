d9e666e14f44c3e824eb82230e570e30b9224b76
stormymayday <stormymayday7@gmail.com>
2023-05-10 21:30:40 -0700

    Moved addToLocalStorage into a separate module.

--- app.js
+++ app.js
@@ -51,21 +51,7 @@ window.addEventListener('DOMContentLoaded', setUpItems);
 // localStorage.removeItem('list');
 
 // Add to local storage - start
-export function addToLocalStorage(id, value) {
 
-    // Setting up the grocery object
-    const grocery = { id: id, value: value };
-
-    // Getting items from the Local Storage
-    let items = getLocalStorage();
-
-    // Adding 'grocery' into the 'items' array
-    items.push(grocery);
-
-    // Putting 'items' into the Local Storage (overriding an existing value)
-    localStorage.setItem('list', JSON.stringify(items));
-
-}
 // Add to local storage - end
 
 // Remove from local storage - start
---
+++ utils/addToLocalStorage.js
@@ -0,0 +1,15 @@
+export default function addToLocalStorage(id, value) {
+
+    // Setting up the grocery object
+    const grocery = { id: id, value: value };
+
+    // Getting items from the Local Storage
+    let items = getLocalStorage();
+
+    // Adding 'grocery' into the 'items' array
+    items.push(grocery);
+
+    // Putting 'items' into the Local Storage (overriding an existing value)
+    localStorage.setItem('list', JSON.stringify(items));
+
+}
