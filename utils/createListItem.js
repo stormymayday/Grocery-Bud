d5b01f5c81f4101a570a2a3ecd8f5153ad69175d
stormymayday <stormymayday7@gmail.com>
2023-05-15 09:04:38 -0700

    Moved createListItem function into a separate module

---
+++ utils/createListItem.js
@@ -0,0 +1,51 @@
+export default function createListItem(id, value) {
+
+    // Creating an article
+    const element = document.createElement('article');
+
+    // Adding the 'grocery-item' class
+    element.classList.add('grocery-item');
+
+    // Adding the ID
+    const attribute = document.createAttribute('data-id');
+    attribute.value = id;
+    element.setAttributeNode(attribute);
+
+    // Setting up the innerHTML
+    element.innerHTML = `                    <span class="title">${value}</span>
+
+                    <!-- btn-container start -->
+                    <div class="btn-container">
+
+                        <!-- edit btn start -->
+                        <button type="button" class="edit-btn">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <!-- edit btn end -->
+
+                        <!-- delete btn start -->
+                        <button type="button" class="delete-btn">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        <!-- delete btn end -->
+
+                    </div>
+                    <!-- btn-container end -->
+        `;
+
+    // Targetting the delete item button
+    const deleteBtn = element.querySelector('.delete-btn');
+
+    // Targetting the edit item button
+    const editBtn = element.querySelector('.edit-btn');
+
+    // delete item button event listener
+    deleteBtn.addEventListener('click', deleteItem);
+
+    // edit item button event listener
+    editBtn.addEventListener('click', editItem);
+
+    // Adding article to the list
+    selections.groceryList.appendChild(element);
+
+}
