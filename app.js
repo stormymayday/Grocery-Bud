ebb48c9f4d0c45638b276276fa345c4630e7fd6c
stormymayday <stormymayday7@gmail.com>
2023-05-14 21:32:31 -0700

    Moved setUpItems into a separate module

--- app.js
+++ app.js
@@ -35,28 +35,7 @@ window.addEventListener('DOMContentLoaded', setUpItems);
 
 // ****** Set Up Items **********
 // setUpItems - start
-function setUpItems() {
 
-    // Getting items from the Local Storage
-    let items = getLocalStorage();
-
-    // Checking if the 'items' array is not empty
-    if (items.length > 0) {
-
-        // Iterating over the items
-        items.forEach(function (item) {
-
-            // Calling the createListItem
-            createListItem(item.id, item.value);
-
-        });
-
-        // Adding the 'show-container' class
-        selections.groceryContainer.classList.add('show-container');
-
-    }
-
-}
 // setUpItems - end
 
 // createListItem - start
---
+++ utils/setUpItems.js
@@ -0,0 +1,22 @@
+export default function setUpItems() {
+
+    // Getting items from the Local Storage
+    let items = getLocalStorage();
+
+    // Checking if the 'items' array is not empty
+    if (items.length > 0) {
+
+        // Iterating over the items
+        items.forEach(function (item) {
+
+            // Calling the createListItem
+            createListItem(item.id, item.value);
+
+        });
+
+        // Adding the 'show-container' class
+        selections.groceryContainer.classList.add('show-container');
+
+    }
+
+}
